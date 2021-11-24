b7cc432fe6016722503cc6cd0cdfd56b6f831c1c
behindthegod <divizionby0@gmail.com>
2021-11-24 21:01:46 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Card, {CardVariant} from "./components/Card";
+import {ITodo, IUser} from "./types/types";
+import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import {ifError} from "assert";
+import TodoItem from "./components/TodoItem";
+import EventExample from "./components/EventExample";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [users, setUsers] = useState<IUser[]>([])
+    const [todos, setTodos] = (useState<ITodo[]>([]))
+
+    useEffect(()=> {
+        fetchUsers()
+        fetchTodos()
+    },[])
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    async function fetchTodos() {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    return (
+        <div>
+            <EventExample/>
+            <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} height='200px'
+                  width='200px'>
+                <button>кнопка</button>
+            </Card>
+            <List items={users} renderItem={(u: IUser) => <UserItem user={u} key={u.id}/>
+            }/>
+            <List items={todos} renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>
+            }/>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react';
+import {IUser} from "../types/types";
+import UserItem from "./UserItem";
+
+interface UserListProps {
+    users: IUser[];
+}
+
+const UserList: FC<UserListProps>= ({users}) => {
+    return (
+        <div>
+            {users.map(u =>
+                <UserItem key={u.id} user={u}/>
+            )}
+        </div>
+    );
+};
+
+export default UserList;
